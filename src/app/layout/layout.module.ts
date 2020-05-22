f6529bd42c0b4c4acd5370a8f0d2fc2c7b3ac080
Luis Fernando <luispoma.frag@gmail.com>
2020-05-22 01:34:38 -0300

    create list component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,13 +5,15 @@ import { LayoutRoutingModule } from './layout-routing.module';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { ListComponent } from './content/list/list.component';
 
 
 @NgModule({
   declarations: [
     ContentComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    ListComponent
   ],
   imports: [
     CommonModule,
