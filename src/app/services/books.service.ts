1c75c30f063c0bbe4d8caa5211a5a4c4af83b1e5
Luis Fernando <luispoma.frag@gmail.com>
2020-05-29 00:00:31 -0300

    create getBookById service

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DataSet } from '../models/DataSet';
 import { environment } from 'src/environments/environment';
+import { BookSearchInfos } from '../models/BookInfos';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +13,13 @@ export class BooksService {
   public getBooks(query: string): Promise<DataSet> {
     return this.http
       .get(`${environment.url}/volumes?q=${query}`)
+      .toPromise()
+      .then((response: any) => response);
+  }
+
+  public getBookById(id: string): Promise<BookSearchInfos> {
+    return this.http
+      .get(`${environment.url}/volumes/${id}`)
       .toPromise()
       .then((response: any) => response);
   }
