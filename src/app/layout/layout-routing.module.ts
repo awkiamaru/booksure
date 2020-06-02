64f3202bd2926415a7b66a6ad09a651ec5efc364
Luis Fernando <luispoma.frag@gmail.com>
2020-06-02 00:47:25 -0300

    create routes and update color background

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout.component';
 import { BookComponent } from './content/book/book.component';
+import { HomeComponent } from './content/home/home.component';
+import { ContentComponent } from './content/content.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'search',
     component: LayoutComponent,
   },
   {
-    path: 'book/:id',
+    path: 'search/book/:id',
     component: BookComponent,
   },
 ];
