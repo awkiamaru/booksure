6e5cf8a82068a5278698f41f5e8d865329c35647
Luis Fernando <luispoma.frag@gmail.com>
2020-06-02 22:15:25 -0300

    add stylize in home page

--- src/app/layout/content/home/home.component.ts
+++ src/app/layout/content/home/home.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  faLongArrowAltRight = faLongArrowAltRight;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
